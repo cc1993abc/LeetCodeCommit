d519b82a7b9a917f8c9b008429fbb782e8a80e91
CC <a18674130101@outlook.com>
2019-04-18 00:18:25 +0800

    1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace hackerrankCommit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+                        Console.WriteLine("Hello World!");
         }
     }
 }
