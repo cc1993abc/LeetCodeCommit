09b7aadd13ac99e77c7dd778986544637ba7d913
陈超(WA03777) <WA03777@worldunion.com.cn>
2019-04-18 14:12:10 +0800

    1

--- Day5.cs
+++ Day5.cs
@@ -11,16 +11,17 @@ namespace hackerrankCommit
             string s4 = string.Empty;
             for (int i = 0; i < n; i++)
             {
-            string s1 = Console.ReadLine();
+            string s1 = "read";
             s3 = string.Empty;
             s4 = string.Empty;
             int index = 0;
             foreach (var s in s1)
             {
                 if(index % 2 == 0){
-                    s3 += s1;
+                    s3 += s;
+                }else{
+                s4 += s;
                 }
-                s4 +=s1;
                 index++;
             }
             System.Console.WriteLine(string.Format("{0}  {1}",s3,s4));  
